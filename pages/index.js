ad4fbf9adc44c27d0778c173dd0f979232670674
Chris on Code <hey@chrisoncode.io>
2020-09-28 17:16:35 -0700

    cleaning

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,32 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-export default function Home({ posts }) {
-  console.log(posts);
-
-  return (
-    <div className="container mx-auto px-10 py-20">
-      <h1 className="text-6xl font-bold mb-20">Welcome to my site</h1>
-
-      {/* posts go here */}
-      {posts.map((post, index) => (
-        <div key={index}>
-          <h2>{post.title}</h2>
-        </div>
-      ))}
-    </div>
-  );
+export default function Home() {
+  return <div>home</div>;
 }
 
-export async function getStaticProps() {
-  const url = process.env.API_URL;
-  const res = await fetch(`${url}/posts`);
-  const posts = await res.json();
+// export default function Home({ posts }) {
+//   console.log(posts);
 
-  return {
-    props: { posts },
-  };
-}
+//   return (
+//     <div className="container mx-auto px-10 py-20">
+//       <h1 className="text-6xl font-bold mb-20">Welcome to my site</h1>
+
+//       {/* posts go here */}
+//       {posts.map((post, index) => (
+//         <div key={index}>
+//           <h2>{post.title}</h2>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export async function getStaticProps() {
+//   const url = process.env.API_URL;
+//   const res = await fetch(`${url}/posts`);
+//   const posts = await res.json();
+
+//   return {
+//     props: { posts },
+//   };
+// }
